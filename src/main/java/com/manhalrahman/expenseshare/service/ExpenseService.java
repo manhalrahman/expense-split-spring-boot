973c304a531804060bee4181dc5ea8bd9197c7c5
manhalrahman <mrmanhal99@gmail.com>
2024-12-15 14:00:35 +0530

    Upating ExpenseService to return a valid PaymentGraph

--- src/main/java/com/manhalrahman/expenseshare/service/ExpenseService.java
+++ src/main/java/com/manhalrahman/expenseshare/service/ExpenseService.java
@@ -1,10 +1,14 @@
 package com.manhalrahman.expenseshare.service;
 
+import com.manhalrahman.expenseshare.entities.Expense;
+import com.manhalrahman.expenseshare.entities.PaymentGraph;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExpenseService {
-
+    public PaymentGraph getPaymentGraph(Expense expense) {
+        return new PaymentGraph();
+    }
 }
